4cf8edac7021aa45e3045271373ca859b9eba737
Maxxxxder <63100573+Maxxxxder@users.noreply.github.com>
2020-06-26 12:00:16 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Who are you?")
+alert("And where are you from?")
+alert("Where do you live?")
+document. write ("Who are you?")
+document. write ("<div>And where are you from?</div>")
+document. write ("<div>Where do you live?</div>")
